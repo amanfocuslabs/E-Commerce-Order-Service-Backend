f78f34d87ab2460b94b9c78fca45e0fa8dc5fb37
aman <amanget4@gmail.com>
2019-07-14 22:32:52 -0500

    Project Starter

---
+++ src/main/java/com/ea/ecommerceorderservicebackend/ECommerceOrderServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ea.ecommerceorderservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOrderServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceOrderServiceBackendApplication.class, args);
+	}
+
+}
